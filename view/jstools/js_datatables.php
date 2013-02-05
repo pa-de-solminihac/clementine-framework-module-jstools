679ac8c88a9775c20df18b99d577ad30c31a2ec9
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:26:23 +0100

    restoring jstools-1.11 branch

--- view/jstools/js_datatables.php
+++ view/jstools/js_datatables.php
@@ -1,3 +1,6 @@
+<?php
+$request = $this->getRequest();
+?>
 <script type="text/javascript">
     // si jQuery est chargé
     if (typeof(jQuery) != "undefined") {
@@ -10,16 +13,10 @@ if (Clementine::$config['module_jstools']['persistent_datatables']) {
                 "bStateSave": true,
 <?php
 }
-if (Clementine::$config['module_jstools']['nb_res_datatables']) {
-?>
-                "aLengthMenu": <?php echo Clementine::$config['module_jstools']['nb_res_datatables']; ?>,
-<?php
-}
                 ?>
-                "aaSorting": [], /* disable initial sort */
                 "sPaginationType": "full_numbers",
                 "oLanguage": {
-                    "sUrl": "<?php echo __WWW_ROOT_JSTOOLS__; ?>/skin/js/jquery.dataTables/locale/<?php echo $request->LANG; ?>.txt"
+                    "sUrl": "<?php echo __WWW_ROOT_JSTOOLS__; ?>/skin/js/jquery.dataTables/locale/<?php echo $request['LANG']; ?>.txt"
                 },
                 "fnDrawCallback": function() {
                     jQuery(this).find('tr').removeClass("alt-row");
